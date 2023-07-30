1e73f836d0002a7b2f7a8ab02db3c2e51f3fdfa1
Robert Brandt <r.e.brandt@gmail.com>
2023-07-30 11:01:20 -0400

    use [] to ref a dict, more pythonic than .get()

--- roman_numerals_lib.py
+++ roman_numerals_lib.py
@@ -9,18 +9,18 @@ dictionary = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 def rom_num_func(value):
     value = value.upper().strip().replace(" ", "")
     if len(value) == 1: 
-        return dictionary.get(value)
+        return dictionary[value]
     sum = 0
     i = 0
     while i < len(value):
         if i == (len(value) - 1):
-            sum = sum + dictionary.get(value[i])
+            sum = sum + dictionary[value[i]]
             i = i + 1
         elif dictionary.get(value[i]) < dictionary.get(value[i+1]):
-            sum = sum + dictionary.get(value[i+1]) - dictionary.get(value[i])
+            sum = sum + dictionary[value[i+1]] - dictionary[value[i]]
             i = i + 2
         else:
-            sum = sum + dictionary.get(value[i])
+            sum = sum + dictionary[value[i]]
             i = i + 1
     return sum
 
@@ -36,12 +36,12 @@ def numerical(value):
         i = int(i)
         div_five = i//5
         if (i == 4) | (i==9):
-            roman_numeral = dict_num.get(multiplier) + dict_num.get((i+1) * multiplier) + roman_numeral
+            roman_numeral = dict_num[multiplier] + dict_num[(i+1) * multiplier] + roman_numeral
         elif div_five == 0:
-            roman_numeral = (i * dict_num.get(multiplier)) + roman_numeral
+            roman_numeral = (i * dict_num[multiplier]) + roman_numeral
         else:
             leftover = i - 5
-            roman_numeral = dict_num.get(5 * multiplier) + (leftover * dict_num.get(multiplier)) + roman_numeral
+            roman_numeral = dict_num[5 * multiplier] + (leftover * dict_num[multiplier]) + roman_numeral
         multiplier = multiplier * 10
     return roman_numeral
 
@@ -68,15 +68,15 @@ def convert(value):
             if len(value) - i >= 3:
                 if (value[i-1] == value[i]) & (value[i] == value[i+1]) & (value[i+1] == value[i+2]):
                     raise ValueError("Invalid roman numeral: 4 or more letter repeats are not allowed.")
-            if (dictionary.get(value[i-1]) * 10) < (dictionary.get(value[i])):
+            if (dictionary[value[i-1]] * 10) < (dictionary[value[i]]):
                 raise ValueError("Invalid roman numeral: '{}{}' invalid prefix decrement / values not decreasing.".format(value[i-1], value[i]))
             if (value[i-1] == value[i]) & ((value[i] == "V" )| (value[i] == "L") | (value[i] == "D")):
                 raise ValueError("Invalid roman numeral: Repeating non-repeatable character {}.".format(value[i]))
-            if ((dictionary.get(value[i-1])) < dictionary.get(value[i])) & ((value[i-1] == "V") | (value[i-1] == "L") | (value[i-1] == "D")):
+            if ((dictionary[value[i-1]]) < dictionary[value[i]]) & ((value[i-1] == "V") | (value[i-1] == "L") | (value[i-1] == "D")):
                 raise ValueError("Invalid roman numeral: Cannot prefix-decrement with {} character.".format(value[i-1]))
             if i == len(value) - 1:
                 continue
-            if (dictionary.get(value[i-1]) == dictionary.get(value[i+1])) & (dictionary.get(value[i-1]) != dictionary.get(value[i])) & ((value[i-1] != "X") & (value[i-1] != "C") & (value[i-1] != "M")):
+            if (dictionary[value[i-1]] == dictionary[value[i+1]]) & (dictionary[value[i-1]] != dictionary[value[i]]) & ((value[i-1] != "X") & (value[i-1] != "C") & (value[i-1] != "M")):
                 raise ValueError("Invalid roman numeral {}: Cannot prefix-decrement and suffix-increment, {} character is doing so.".format(value, value[i-1]))
         else:
             raise ValueError("Roman numerals are only comprised of {} characters.".format(dictionary.keys()))
